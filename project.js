9b4abb1279c2d4bf6a256f525d8f010373517d08
aethlis <84690253+aethlis@users.noreply.github.com>
2021-06-30 12:36:46 +0900

    finally finished JS for the project selector page

--- project.js
+++ project.js
@@ -7,7 +7,7 @@ var projects = [
             {
                 project_link: "./projectpage.html",
                 project_infoname: "거캠 20/3모듈 주제중심",
-                project_img: "./",
+                project_img: "./images/2020_3M_MTP/주제공감.jpg",
             }
         ]
     },
@@ -30,7 +30,7 @@ var projects = [
         info_data: [
             {
                 project_link: "./",
-                project_infoname: "거캠 20/4모듈 문제정의",
+                project_infoname: "거캠 20/4모듈 주제중심",
                 project_img: "./"
             }
         ]
@@ -66,7 +66,7 @@ var projects = [
         info_data: [
             {
                 project_link: "./",
-                project_infoname: "21/1모듈 알파랩 - CLab",
+                project_infoname: "21/2모듈 알파랩 - CLab",
                 project_img: "./"
             }
         ]
@@ -109,18 +109,6 @@ var projects = [
     },
 
     {
-        project_name: ".음악동아리",
-        project_type: "After School",
-        info_data: [
-            {
-                project_link: "./",
-                project_infoname: "CJ 도너스캠프 음악동아리",
-                project_img: "./"
-            }
-        ]
-    },
-
-    {
         project_name: ".Album 제작",
         project_type: "After School",
         info_data: [
@@ -134,10 +122,19 @@ var projects = [
 
 ]
 
+{/* <div class="scrollTitle"><a href="${date.project_link}">${project.project_name}<a></div> */}
+
+
+//scrollContents에 해당하는 태그 모두 불러오기
+var el = document.getElementsByClassName('scrollContents');
+var hoverInfo = document.getElementById('hoverInfo')
+var hoverTitle = document.getElementById('hoverTitle')
+var hoverImage = document.getElementById('hoverImage')
+
 for (var i in projects) {
     var project = projects[i]
     for (var j in project.info_data) {
-        var date = project.info_data[j]
+        var infodata = project.info_data[j]
         document.querySelector(".scrollSelector").innerHTML += `
         <div class="scrollContents" id="data-${i}">
             <div class="scrollTitle">${project.project_name}</div>
@@ -146,17 +143,46 @@ for (var i in projects) {
         `;
     }
 }
-{/* <div class="scrollTitle"><a href="${date.project_link}">${project.project_name}<a></div> */}
 
-// for (var k in projects) {   
-//     var el  = document.getElementById(`data-${k}`);
-//     var data = Object.assign({}, projects[k]);
-    
-//     el.addEventListener('click', function() {
-//         console.log(data);
-//         alert(JSON.stringify(data))
-//     })
-// }
+//forEach 반복문을 사용해 유사배열의 각각의 요소에 data 할당
+//참고 : https://www.zerocho.com/category/JavaScript/post/5af6f9e707d77a001bb579d2
+
+Array.prototype.forEach.call(el, function (item, index) {
+    item.addEventListener('mouseover', function () {
+        hoverInfo.style.display = "flex"
+
+        var data = Object.assign({}, projects[index].info_data);
+        var image = projects[index].info_data[0].project_img
+        var title = projects[index].info_data[0].project_infoname
+        var link = projects[index].info_data[0].project_link
+        
+        hoverTitle.innerHTML = title
+        hoverImage.innerHTML = `<a href="${link}">
+                                <img src="${image}" alt="" id="images">
+                                </a>`
+        
+    })
+});
+
+Array.prototype.forEach.call(el, function (item, index) {
+    item.addEventListener('mouseout', function () {
+        // var data = Object.assign({}, projects[index]);
+
+        for (var i in projects) {
+            var project = projects[i]
+            for (var j in project.info_data) {
+                var infodata = project.info_data[j]
+                hoverInfo.style.display = "none";
+
+                hoverTitle.innerHTML = ""
+                hoverImage.innerHTML = ""
+                return
+            }
+            return
+        }
+    })
+});
+
 
 
 
